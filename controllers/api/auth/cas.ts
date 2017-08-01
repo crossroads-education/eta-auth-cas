9879aff65dba4040286572fbb3b712f49a12eed6
Alex Hicks <aldahick@iupui.edu>
2017-08-01 12:43:21 -0400

    Port CAS auth provider to Eta v2.1

---
+++ controllers/api/auth/cas.ts
@@ -0,0 +1,24 @@
+import * as eta from "../../../eta";
+import * as db from "../../../db";
+
+@eta.mvc.route("/api/auth/cas")
+@eta.mvc.controller()
+export default class ApiAuthCasController extends eta.IHttpController {
+    @eta.mvc.raw()
+    @eta.mvc.post()
+    @eta.mvc.params(["firstName", "lastName", "email"])
+    public async register(firstName: string, lastName: string, email: string): Promise<void> {
+        if (!this.req.session["casUsername"]) {
+            this.res.statusCode = eta.constants.http.AccessDenied;
+            return;
+        }
+        const person: db.Person = new db.Person({
+            firstName,
+            lastName,
+            username: this.req.session["casUsername"],
+            email
+        });
+        await db.person().save(person);
+        this.redirect("/auth/cas/login");
+    }
+}
